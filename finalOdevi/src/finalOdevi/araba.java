3d8a5e677f2881d313dd6c66ac80e0a63d721d84
fatma <fatma@fatma>
2022-12-08 12:28:32 +0300

    first commit

---
+++ finalOdevi/src/finalOdevi/araba.java
@@ -0,0 +1,22 @@
+package finalOdevi;
+
+public class araba {
+String marka;
+public void showInfo() {
+	
+}
+public void getMarka(String marka ) {
+	
+}public void setMarka(String pMarka) {
+this.marka=pMarka;
+}
+
+class araba1 extends araba{
+	
+}
+
+
+
+
+
+}
